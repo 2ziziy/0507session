a7c463996899d29a8581f48fd12db624122225ea
zi <2ziziy@naver.com>
2025-05-12 12:22:51 +0900

    src

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import TimerDisplay from "./TimerDisplay";
+import TimerButton from "./TimerButton";
+
+function App() {
+  const [seconds, setSeconds] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+
+    if (isRunning) {
+      interval = setInterval(() => {
+        setSeconds(prev => {
+          if (prev === 20) {
+            clearInterval(interval);
+            setIsRunning(false);
+            alert("20초가 지났습니다!");
+            return 20;
+          }
+          return prev + 1;
+        });
+      }, 1000);
+    }
+
+    return () => clearInterval(interval);
+  }, [isRunning]);
+
+  const handleStart = () => setIsRunning(true);
+  const handleStop = () => setIsRunning(false);
+  const handleReset = () => {
+    setIsRunning(false);
+    setSeconds(0);
+    console.log("리셋되었습니다.")
+  };
+
+  return (
+    <div className="container">
+      <TimerDisplay seconds={seconds} />
+      <TimerButton onStart={handleStart} onStop={handleStop} reset={handleReset} />
+    </div>
+  );
+}
+
+export default App;
